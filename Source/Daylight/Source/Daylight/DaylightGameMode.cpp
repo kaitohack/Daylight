2541418e4cac0dff2aa5911dd1cd75ca1514aa5f
unknown <agarcia@Alvaro-W7.vt.dom>
2015-03-25 13:49:50 +0100

    U4 Project

---
+++ Source/Daylight/Source/Daylight/DaylightGameMode.cpp
@@ -0,0 +1,8 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Daylight.h"
+#include "DaylightGameMode.h"
+
+
+
+
---
+++ Source/Daylight/Source/Daylight/DaylightGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "DaylightGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DAYLIGHT_API ADaylightGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
